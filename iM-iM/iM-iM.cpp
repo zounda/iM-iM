308fd554b2381c80c5333c2c6989841f97175723
benjamin <benjamin_bakiu@hotmail.com>
2011-02-10 02:07:17 +0100

    CMD-Chat

---
+++ iM-iM/iM-iM.cpp
@@ -0,0 +1,16 @@
+// Autor: Benjamin Bakki & Freddy Grodtke
+// Programmname: iM-instant Messagingv(iM-iM)
+// Programmart: CMD-Chat
+// Version: v0.0a
+// Datum: 09.10.2011
+
+#include "stdafx.h"
+#include <iostream>
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	system ("PAUSE");
+	return 0;
+}
+
